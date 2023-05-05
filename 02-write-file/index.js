b78bbe478f46f7696d4297a556022cfb5426d8d6
Roshenchik <asalex2001@mail.ru>
2023-05-06 02:12:20 +0300

    feat: solve third task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const {stdin, stdout} = process;
-const pathToFile = path.join(__dirname, 'input.txt')
+const pathToFile = path.join(__dirname, 'input.txt');
 
 const inputText = () => {
 	stdout.write('Type some text\n');
 	stdin.on('data', data => {
 
 		if (data.toString().trim() === 'exit') {
-			process.exit()
+			process.exit();
 		}
 
 		fs.appendFile(pathToFile, data, (err) => {
@@ -24,12 +24,12 @@ fs.writeFile(pathToFile, '', (err) => {
 	} else {
 		inputText();
 	}
-})
+});
 
 process.on('SIGINT', function() {
 	process.exit();
 });
 process.on('exit', () => {
-	stdout.write('Goddamn, great input bro!') 
+	stdout.write('Goddamn, great input bro!');
 });
 
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(dirPath, (err, files) => {
+	if (err) {
+		throw err;
+	} else {
+		files.forEach((file) => {
+			fs.stat(dirPath + `/${file}`, (err, stats) => {
+				if (!stats.isFile()) return;
+				const name = path.parse(dirPath + `/${file}`).name;
+				const extension = (path.parse(dirPath + `/${file}`).ext.slice(1));
+				const size = stats.size / 1000 + ' kb';
+				console.log(`${name} - ${extension} - ${size}`);
+			});
+		});
+	}
+});
