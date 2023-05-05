c3f807f3bb8e8e771ba9765fa54a27c8ba2750a0
Roshenchik <asalex2001@mail.ru>
2023-05-05 03:04:21 +0300

    feat: solve two task

---
+++ 02-write-file/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const {stdin, stdout} = process;
+const pathToFile = path.join(__dirname, 'input.txt')
+
+const inputText = () => {
+	stdout.write('Type some text\n');
+	stdin.on('data', data => {
+
+		if (data.toString().trim() === 'exit') {
+			process.exit()
+		}
+
+		fs.appendFile(pathToFile, data, (err) => {
+		if (err) throw err;
+		})
+
+	});
+}
+
+fs.writeFile(pathToFile, '', (err) => {
+	if (err) {
+		console.log('File already has been created');
+	} else {
+		inputText();
+	}
+})
+
+process.on('SIGINT', function() {
+	process.exit();
+});
+process.on('exit', () => {
+	stdout.write('Goddamn, great input bro!') 
+});
+
